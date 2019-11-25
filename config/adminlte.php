65c349dde071439e15a92d39919e2b9823eb44bc
Esteban <stbn.j.17@gmail.com>
2019-11-25 12:27:57 -0500

    Probando Modif Master

--- config/adminlte.php
+++ config/adminlte.php
@@ -1,20 +1,18 @@
-return [
-    'Theme' => [
-        'title' => 'AdminLTE',
-        'logo' => [
-            'mini' => '<b>A</b>LT',
-            'large' => '<b>Admin</b>LTE'
-        ],
-        'login' => [
-            'show_remember' => true,
-            'show_register' => true,
-            'show_social' => true
-        ],
-        'folder' => ROOT,
-        'skin' => 'yellow'
-    ]
-];
-
-// config/bootstrap.php
-
-Configure::load('adminlte', 'default');
+<?php 
+    return [
+        'Theme' => [
+            'title' => 'AdminLTE',
+            'logo' => [
+                'mini' => '<b>A</b>LT',
+                'large' => '<b>Admin</b>LTE'
+            ],
+            'login' => [
+                'show_remember' => true,
+                'show_register' => true,
+                'show_social' => true
+            ],
+            'folder' => ROOT,
+            'skin' => 'yellow-light'
+        ]
+    ];
+?>
